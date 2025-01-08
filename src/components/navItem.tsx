9e0d9cb1733aeec257ae42c185f024814bb079f7
leconstantin <lcon69184@gmail.com>
2025-01-08 17:26:15 +0200

    added suspense for deployment errors

--- src/components/navItem.tsx
+++ src/components/navItem.tsx
@@ -6,6 +6,7 @@ import { usePathname, useSearchParams } from "next/navigation";
 import { buttonVariants } from "@/components/ui/button";
 
 import { cn } from "@/lib/utils";
+import { Suspense } from "react";
 
 interface Props
   extends Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, "href">,
@@ -21,15 +22,17 @@ export function NavItem({ href, tag, className, ...props }: Props) {
   const isActive = pathname === href || tagParam === tag;
 
   return (
-    <Link
-      href={href}
-      className={cn(
-        buttonVariants({ variant: "ghost" }),
-        "w-full justify-start space-x-3 px-2 xl:px-3",
-        isActive && "bg-accent text-accent-foreground",
-        className
-      )}
-      {...props}
-    />
+    <Suspense>
+      <Link
+        href={href}
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "w-full justify-start space-x-3 px-2 xl:px-3",
+          isActive && "bg-accent text-accent-foreground",
+          className
+        )}
+        {...props}
+      />
+    </Suspense>
   );
 }
